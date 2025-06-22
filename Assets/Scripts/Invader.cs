58579fa46bc47f6bc8df92a035e204f3b86758ec
Berserk-Alert <111589429+Berserk-Alert@users.noreply.github.com>
2025-06-21 22:37:05 -0700

    scripts cont. ch 3

---
+++ Assets/Scripts/Invader.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Invader : MonoBehaviour
+{
+    public Sprite[] animationSprites = new Sprite[0];
+    public float animationTime = 1.0f;
+
+    private SpriteRenderer spriteRenderer;
+    private int animationFrame;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = animationSprites[0];
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), animationTime, animationTime);
+    }
+
+    private void AnimateSprite()
+    {
+        animationFrame++;
+
+        // Loop back to the start if the animation frame exceeds the length
+        if (animationFrame >= animationSprites.Length) {
+            animationFrame = 0;
+        }
+
+        spriteRenderer.sprite = animationSprites[animationFrame];
+    }
+
+}
